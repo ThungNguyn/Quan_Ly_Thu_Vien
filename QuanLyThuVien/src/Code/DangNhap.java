59f79bcc274dec54dd66730180fab07eb7c25562
ThungNguyn <thuongnguyen.03052003@gmail.com>
2024-07-29 15:53:16 +0700

    QLTV

---
+++ QuanLyThuVien/src/Code/DangNhap.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Code;
+
+import GiaoDien.JF_DangNhap;
+
+/**
+ *
+ * @author DELL
+ */
+public class DangNhap {
+    public static String idnv = null;    
+    public static String tennv = null;
+
+    public static void main(String[] args) {
+        System.out.println("Day la code form dang nhap");
+        GiaoDien.JF_DangNhap dn = new JF_DangNhap();
+        dn.setLocationRelativeTo(null);
+        dn.setVisible(true);
+//        
+    }
+}
